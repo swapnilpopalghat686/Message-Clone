828e4c8208ce08d0fdf0298f0acd46cc91c97002
swapnilpopalghat686 <swapnilpopalghat44@gmail.com>
2025-08-24 17:21:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("");
+
+  if (isSent) {
+    return (
+      <div className="center">
+        <div className="box">
+          <h1 className="" style={{color:'green'}}>Your message is Sent to Your Special One!</h1>
+        </div>
+      </div>
+    );
+  }
+
+  // count words (ignores extra spaces)
+  const wordCount = message.trim().split(/\s+/).filter(Boolean).length;
+
+  return (
+    <div className="center">
+      <div className="box">
+        <h2 className="heading" style={{color:'blue'}}>Message Your Someone</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setIsSent(true);
+            sendMessage(message);
+          }}
+        >
+          <textarea
+            placeholder="Type Something !!"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="textarea"
+          />
+          <br />
+          <button
+            type="submit"
+            className={`btn ${wordCount >= 2 ? "visible" : "hidden"}`}
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function sendMessage(message) {
+  console.log("Message sent:", message);
+  alert('Please Confirm Your Massage!!')
+}
